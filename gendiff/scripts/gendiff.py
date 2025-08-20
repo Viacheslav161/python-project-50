db47c571bfa262401d2ebd33e72f10e86c38ac36
Viacheslav <shtirlizz@yandex.ru>
2025-08-20 22:22:23 +0300

    6+ step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,37 +1,32 @@
-print("Running gendiff/scripts/gendiff.py")
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 
 import argparse
-from gendiff.parser import load_data
-def generate_diff(file1_path, file2_path):
-    print(f"Loading files: {file1_path}, {file2_path}")
-    data1 = load_data(file1_path)
-    data2 = load_data(file2_path)
-    keys1 = set(data1.keys())
-    keys2 = set(data2.keys())
-    all_keys = sorted(keys1.union(keys2))
-    diff = "{\n"
-    for key in all_keys:
-        if key in data1 and key in data2:
-            if data1[key] != data2[key]:
-                diff += f"  - {key}: {data1[key]}\n"
-                diff += f"  + {key}: {data2[key]}\n"
-        elif key in data1:
-            diff += f"  - {key}: {data1[key]}\n"
-        elif key in data2:
-            diff += f"  + {key}: {data2[key]}\n"
-    diff += "}"
-    return diff
+
+from gendiff.scripts.generate_diff import generate_diff
+
+
+def parser_function():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument("-f", "--format",
+                        help='set format of output',
+                        default='stylish', type=str
+    )
+
+    return parser.parse_args()
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
-    parser.add_argument("first_file", help="First file to compare")
-    parser.add_argument("second_file", help="Second file to compare")
-    parser.add_argument("-f", "--format", default="plain", help="Set format of output")
-    args = parser.parse_args()
-    diff = generate_diff(args.first_file, args.second_file)
-    print(diff)
-if __name__ == '__main__':
+    args = parser_function()
+
+    file_path1 = args.first_file
+    file_path2 = args.second_file
+
+    result = generate_diff(file_path1, file_path2, formatter=args.format)
+    print(result)
+
+
+if __name__ == "__main__":
     main()
