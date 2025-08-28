d8cc889e4c7f1e75dc299d76ac91bf871d1571cb
Viacheslav <shtirlizz@yandex.ru>
2025-08-28 16:51:27 +0300

    ruff check

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -2,6 +2,7 @@ from gendiff.formatters.format_identifier import format_identifier
 from gendiff.scripts.find_diff import find_diff
 from gendiff.parser import parse_data_from_file
 
+
 def generate_diff(file_path1, file_path2, formatter='stylish'):
     first_file = parse_data_from_file(file_path1)
     second_file = parse_data_from_file(file_path2)
