92b4099526e0858dd080d676517bc7e580016872
Viacheslav <shtirlizz@yandex.ru>
2025-09-22 17:51:44 +0300

    test_gendiff add line

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,6 +3,7 @@ import pytest
 from gendiff.scripts.generate_diff import generate_diff
 from gendiff.scripts.parser import read_file
 
+
 @pytest.mark.parametrize('file_path1, file_path2, expected_result', [
     ('tests/test_data/file1.json',
      'tests/test_data/file2.json',
