09857dda10022ab20a4ccd77850c517d3d1184ff
Viacheslav <shtirlizz@yandex.ru>
2025-09-22 17:28:01 +0300

    11

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -1,6 +1,6 @@
 from gendiff.formatters.format_identifier import format_identifier
 from gendiff.scripts.find_diff import find_diff
-from gendiff.parser import parse_data_from_file
+from gendiff.scripts.parser import parse_data_from_file
 
 
 def generate_diff(file_path1, file_path2, formatter='stylish'):
